42a8d4b77c475eb3c541ec02fd52e740e64389f0
Danil <Danil122999@gmail.com>
2023-02-18 15:37:13 +0600

    Домашнее задание 2

---
+++ task10/Program.cs
@@ -0,0 +1,56 @@
+﻿// Console.Write("Введите четверть: " );
+// int X = int.Parse(Console.ReadLine()!);
+
+// if(X == 1)
+// {
+//     Console.WriteLine("Диапазон х от 0 до 5 и диапазон y от 0 до 5");
+// }
+// if(X == 2)
+// {
+//     Console.WriteLine("Диапазон x от -5 до 0 и диапазн y от 0 до 5");
+// }
+// if(X == 3)
+// {
+//     Console.WriteLine("Диапазон x от -5 до 0 и диапазн y от -5 до 0");
+// }
+// if(X == 4)
+// {
+//     Console.WriteLine("Диапазон x от 0 до 5 и диапазн y от -5 до 0");
+// }
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+//AB = √(xb - xa)2 + (yb - ya)2
+
+// Console.Write("Введите координаты X1 : ");
+// int X1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите координаты Y1 : ");
+// int Y1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите координаты X2 : ");
+// int X2 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите координаты Y2 : ");
+// int Y2 = int.Parse(Console.ReadLine()!);
+// int A = (X2 - X1)*(X2 - X1);
+// int B = (Y2 - Y1)*(Y2 - Y1);
+// double AB = Math.Sqrt(A + B);
+// Console.Write(Math.Round (AB, 2));
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+Console.Write("Введите число : ");
+int N = int.Parse(Console.ReadLine()!);
+int x = 1;
+while(x <= N)
+{
+    int y = x*x;
+    Console.WriteLine(y);
+    x++;
+}
---
+++ Домашнее задание 2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Console.Write("Введите пятизначное число : " );
+// int num = int.Parse(Console.ReadLine()!);
+// if(num / 10000 == num % 10 && num % 10000 / 1000 == num % 100 / 10)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Console.Write("Введите координаты X1 : ");
+// int X1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите координаты Y1 : ");
+// int Y1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите координаты X2 : ");
+// int X2 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите координаты Y2 : ");
+// int Y2 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите координаты Z1 : ");
+// int Z1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите координаты Z2 : ");
+// int Z2 = int.Parse(Console.ReadLine()!);
+// int A = (X2 - X1) * (X2 - X1);
+// int B = (Y2 - Y1) * (Y2 - Y1);
+// int C = (Z2 - Z1) * (Z2 - Z1);
+// double ABC = Math.Sqrt(A + B + C);
+// Console.WriteLine(Math.Round(ABC, 2));
+
+// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число : ");
+int N = int.Parse(Console.ReadLine()!);
+int x = 1;
+while(x <= N)
+{
+    int y = x*x*x;
+    Console.WriteLine(y);
+    x++;
+}
