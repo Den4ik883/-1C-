5e2d81faaba5221a64786abbd60e6b264c046186
Danil <Danil122999@gmail.com>
2023-03-06 16:57:57 +0600

    Домашнее задание5

---
+++ task13/Program.cs
@@ -0,0 +1,82 @@
+﻿
+// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+// int[,] GetArray(int m, int n, int minValue, int maxValue)
+// {
+//     int[,] result = new int[m,n];
+//     for(int i = 0; i < m; i++)
+//     {
+//         for(int j = 0; j < n; j++)
+//         {
+//             result[i,j] = i + j;
+//             Console.Write($"{result[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+//     return result;
+// }
+// Console.Write("Введите количество строк массива: ");
+// int rows =int.Parse(Console.ReadLine()!);
+// Console.Write("Введите количество строк массива: ");
+// int columns =int.Parse(Console.ReadLine()!);
+// int[,] array = GetArray(rows,columns, 0, 10);
+int[,] GetArray(int rows, int columns)
+{
+    int[,] res = new int [rows,columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            res[i,j] = new Random().Next(1,9);
+        }
+    }
+    return res;
+}
+void PrintArray(int[,] inArray)
+{
+    for(int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++) 
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();   
+    }
+}
+void GetSumInd(int[,] inArray)
+{
+    int sum = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if(i == j)
+            {
+                sum = sum + inArray[i,j];
+            }
+        }        
+    }
+    Console.WriteLine($"Сумма элементов диагонали равна: {sum}");
+}
+Console.Write("Введите количество строк массива: ");
+int rows =int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество строк массива: ");
+int columns =int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(rows,columns);
+PrintArray(array);
+Console.WriteLine();
+GetSumInd(array);
+//PrintArray(array, rows, columns);
+
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
---
+++ Домашнее Задание 5/Program.cs
@@ -0,0 +1,148 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+// double[,] GetArray(int m, int n)
+// {
+//     double[,] result = new double[m,n];
+//     for(int i = 0; i < m; i++)
+//     {
+//         for(int j = 0; j < n; j++)
+//         {
+//             result[i,j] = new Random().NextDouble();
+//             Console.Write($"{Math.Round(result[i,j], 2)} ");
+//         }
+//         Console.WriteLine();
+//     }
+//     return result;
+// }
+// Console.Write("Введите количество строк массива: ");
+// int rows =int.Parse(Console.ReadLine()!);
+// Console.Write("Введите количество строк массива: ");
+// int columns =int.Parse(Console.ReadLine()!);
+// double[,] array = GetArray(rows,columns);
+
+
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// [1,7] -> по данным индексам такого числа в массиве нет
+
+
+// int[,] GetArray(int rows, int col)
+// {
+//     int[,] res = new int[rows, col];
+//     for(int i = 0; i < rows; i++)
+//     {
+//         for(int j = 0; j < col; j++)
+//         {
+//             res[i,j] = new Random().Next(1, 9);
+//         }
+//     }
+//     return res;
+// }
+
+// void PrintArray(int[,] res)
+// {
+//     for (int i = 0; i < res.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < res.GetLength(1); j++)
+//         {
+//             Console.Write($"{res[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// void IndexSearch(int[,] res)
+// {
+//     Console.WriteLine($"Введите индекс строки: ");
+//     int indexOne = Convert.ToInt32(Console.ReadLine()!);
+//     Console.WriteLine($"Введите индекс столбца: ");
+//     int indexTwo = Convert.ToInt32(Console.ReadLine()!);
+//     if(indexOne > res.GetLength(0) || indexTwo > res.GetLength(1))
+//     {
+//         Console.WriteLine($"[{indexOne},{indexTwo}] - по таким координатом числа нет в массиве");
+        
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{res[indexOne, indexTwo]} - число по заданым координатам");
+//     }
+// }
+// Console.WriteLine($"Введите колличество строк массива: ");
+// int rows = Convert.ToInt32(Console.ReadLine()!);
+// Console.WriteLine($"Введите колличество строк массива: ");
+// int columns = Convert.ToInt32(Console.ReadLine()!);
+// int[,] array = GetArray(rows,columns);
+// PrintArray(array);
+// IndexSearch(array);
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetArray(int rows, int col)
+{
+    int[,] res = new int[rows, col];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            res[i,j] = new Random().Next(1, 9);
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] res)
+{
+    for (int i = 0; i < res.GetLength(0); i++)
+    {
+        for (int j = 0; j < res.GetLength(1); j++)
+        {
+            Console.Write($"{res[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void AverageIndex(int[,] res)
+{
+    double average = 0;
+    double sum = 0;
+    for(int col = 0; col < res.GetLength(1); col++)
+    {
+        for(int rows = 0; rows < res.GetLength(0); rows++)
+        {
+            sum = sum + res[rows,col];
+        }
+        average = sum / res.GetLength(0);
+        Console.Write($"{Math.Round(average,2)} ;");
+    }
+}
+Console.WriteLine($"Введите колличество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine($"Введите колличество строк массива: ");
+int columns = Convert.ToInt32(Console.ReadLine()!);
+int[,] array = GetArray(rows,columns);
+PrintArray(array);
+Console.WriteLine();
+AverageIndex(array);
